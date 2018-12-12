2600ef1dd910d83f93fd3b95cdda46829bb0e093
Bear <william456821@gmail.com>
2018-12-12 13:32:09 +0800

    Update : 更改for 迴圈 及多一些說明

--- calculate.js
+++ calculate.js
@@ -3,28 +3,27 @@ let data = fs.readFileSync('Lena.raw')
 let array = new Array(512)
 array.fill(0)
 let count = 0;
-let lastValue = 0
+let len = data.length
 
-/* 一般的方法
-for(let key in data){ //還沒跳脫最後那些東西
-    let value = data[key]
-    if(typeof value != 'number' || key == 'offset')
-        continue
+ //一般的方法 的 entropy
+ 
+for(let i = 0 ; i < len ; i++){ 
+    let value = data[i]
     array[value]++
     count++
-}*/
+}
 
-for(let key in data){ //還沒跳脫最後那些東西
-    let value = data[key]
-    if(typeof value != 'number' || key == 'offset')
-        continue
+/// DPCM 的 entropy
+/*
+let lastValue = 0
+for(let i = 0 ; i < len ; i++){
+    let value = data[i]
     let t = value - lastValue
     lastValue = value
     array[t+256]++
     count++
-}
+}*/
 
-console.log()
 let sum = 0
 array.forEach((e,i)=>{
     if(e==0)
